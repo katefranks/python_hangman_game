d571c69d0df0fa511a5a22a1786fcc103a685233
katefranks <kmfranks9@gmail.com>
2021-06-08 17:02:25 -0400

    updated to replace index of list w/ correct guesses

--- hangman.py
+++ hangman.py
@@ -1,8 +1,11 @@
 def hangman():
     word = "dulce"
+    word_list = ["d", "u", "l", "c", "e"]
+    word_list2 = ["_", "_", "_", "_", "_"]
     guesses = ""
     turns = 10
     failed = 0
+
 
     while turns > 0:
         guess = input("Guess a letter! ")
@@ -11,6 +14,20 @@ def hangman():
             turns -= 1
             guesses += guess
             print(f"Good guess! You have {turns} guesses left. {guesses}")
+            print(word_list2)
+
+
+            for index, char in enumerate(word_list):
+                # if the char is the guess
+                # then update the index in the word_list2 to be that char
+                if char == guess:
+                    word_list2[index] = guess
+                    print(word_list2)
+
+
+
+
+
         elif guess not in word:
             failed += 1
             turns -= 1
@@ -25,7 +42,9 @@ def hangman():
 hangman()
 
 
-
+#figure out all of the places that the char exists in the word
+#find all of the positiions of where it's in the word
+#update the correct position in the list to be that guess
 
 
 
